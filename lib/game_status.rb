96962013f60b32f2676089c6acdab5cfa67b0eca
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-02-02 21:26:56 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-def won(win_combinations)
+def won(board)
   for each win_combinations in WIN_COMBINATIONS
     win_index_1 = win_combinations[0]
     win_index_2 = win_combinations[1]
