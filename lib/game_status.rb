831ff835425bae1a13dcf44db8995cc4e0aec390
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-02-02 21:35:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,5 +7,18 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won(board)
+  WIN_COMBINATIONS.each do |win_combinations|
+    win_index_1 = win_combinations[0]
+    win_index_2 = win_combinations[1]
+    win_index_3 = win_combinations[2]
 
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    else
+      return false
+    end
 end
